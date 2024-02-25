37fcbba833a001b17e6f73b6b532db9a59b93c11
cfLaz <jankoviclaz@gmail.com>
2024-02-25 23:18:33 +0100

    create ts interface for api response

--- src/Contexts.tsx
+++ src/Contexts.tsx
@@ -1,32 +1,37 @@
 import { ReactNode, createContext, useState } from 'react'
+import { QuestionData } from './api/getQuestions'
 
 interface QandAContextValues {
-   allQuestionsData: any[]
-   setAllQuestionsData: (data: any[]) => void
-   currentQuestionData: any
-   setCurrentQuestionData: (data: any) => void
-   userAnswer: string
+   allQuestionsData: QuestionData[]
+   setAllQuestionsData: (data: QuestionData[]) => void
+   currentQuestionData: QuestionData
+   setCurrentQuestionData: (data: QuestionData) => void
+   userAnswer?: string
    giveAnswer: (answer: string) => void
 }
 
 export const QandAContext = createContext<QandAContextValues>({
    allQuestionsData: [],
    setAllQuestionsData: () => {},
-   currentQuestionData: {},
+   currentQuestionData: {} as QuestionData,
    setCurrentQuestionData: () => {},
    userAnswer: '',
    giveAnswer: () => {},
 })
 
 export const QuestionsAndAnswers = ({ children }: { children: ReactNode }) => {
-   const [allQuestionsData, setQData] = useState([{}])
-   const [currentQuestionData, setCurrentQData] = useState({})
-   const [userAnswer, setUserAnswer] = useState('')
+   const [allQuestionsData, setQData] = useState<QuestionData[]>([])
+   const [currentQuestionData, setCurrentQData] = useState<QuestionData>(
+      {} as QuestionData
+   )
+   const [userAnswer, setUserAnswer] = useState<string>()
 
-   const setAllQuestionsData = (allQuestionsData: any[]) =>
+   const setAllQuestionsData = (allQuestionsData: QuestionData[]) =>
       setQData(allQuestionsData)
-   const setCurrentQuestionData = (questionData: {}) =>
+
+   const setCurrentQuestionData = (questionData: QuestionData) =>
       setCurrentQData(questionData)
+
    const giveAnswer = (answer: string) => setUserAnswer(answer)
 
    return (
--- src/api/getQuestions.ts
+++ src/api/getQuestions.ts
@@ -4,13 +4,25 @@ import { decodeBase64Object } from './../util/decodeBase64Object'
 
 type Difficulty = 'easy' | 'medium' | 'hard'
 
-export async function getRandomQuestions(difficulty: Difficulty, amount = 5) {
+export interface QuestionData {
+   type: 'multiple' | 'boolean'
+   difficulty: Difficulty
+   category: string
+   question: string
+   correct_answer: string
+   incorrect_answers: string[]
+}
+
+export async function getRandomQuestions(
+   difficulty: Difficulty,
+   amount = 5
+): Promise<QuestionData[]> {
    if (amount < 1 || amount > 20) amount = 5
    const response = await getWithParams({
       difficulty: difficulty,
       amount: amount,
    })
-   let results = response.data.results.map((question: any) =>
+   let results: QuestionData[] = response.data.results.map((question: any) =>
       decodeBase64Object(question)
    )
    return results
