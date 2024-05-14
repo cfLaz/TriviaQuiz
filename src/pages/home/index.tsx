550ab9f987794abf5eb48425d2d10959cb7c13fd
cfLaz <jankoviclaz@gmail.com>
2024-05-14 23:43:42 +0200

    use logo and change background on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,33 @@
+// import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import testpic from '../../assets/pictures/TriviaQuiz.png'
 
 export function Home() {
+   // return (
+
+   /*    const [position, setPosition] = useState({ x: 0, y: 0 })
+
+   const handleMouseMove = (event: React.MouseEvent) => {
+      setPosition({
+         x: event.clientX,
+         y: event.clientY,
+      })
+   }
+ */
    return (
-      <div className='homepage'>
-         <Link to='quiz'>Go to quiz</Link>
+      <div className='homepage' /* onMouseMove={handleMouseMove} */>
+         {/* <div
+            className='cursorLight'
+            style={{
+               top: `${position.y}px`,
+               left: `${position.x}px`,
+            }}
+         /> */}
+         <div className='header'>
+            <img className='logo' src={testpic}></img>
+         </div>
+
+         {/* <Link to='quiz'>Go to quiz</Link> */}
       </div>
    )
 }
