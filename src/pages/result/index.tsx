4043e12d883c09a41a5e013a51079a8d58b51499
cfLaz <jankoviclaz@gmail.com>
2024-10-03 22:33:09 +0200

    create test/temporary result page table

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,10 +1,29 @@
+import { useSelector } from 'react-redux'
+import { QuestionsStateProps } from '../../store/QuestionsController'
+import { ResultHeader, ResultRow } from './ResultRow'
+import finishedQuizData from './finishedQuizData'
+import { QuestionData } from '../../api/getQuestions'
+
 const Result = () => {
+   //    const { allQuestionsData } = useSelector(
+   //       (state: { QuestionsState: QuestionsStateProps }) => state.QuestionsState
+   //    )
 
-    //need to create another store reducer for stats, wo we can show them here
+   const allQuestionsData = finishedQuizData() as any
+   return (
+      <div id='resultTable'>
+         <table>
+            <ResultHeader />
+            <tbody>
+               {allQuestionsData.map((qd: any) => (
+                  <ResultRow data={qd} />
+               ))}
+            </tbody>
 
-    return (
-        <div>Congratulations!</div>
-    )
+            <tfoot></tfoot>
+         </table>
+      </div>
+   )
 }
 
-export default Result;
+export default Result
