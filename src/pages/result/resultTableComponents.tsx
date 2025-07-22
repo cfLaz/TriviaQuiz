caa1cc670cfe56da1342edbba504fca9a971b64e
cfLaz <jankoviclaz@gmail.com>
2025-07-22 23:45:50 +0200

    add category column to result table and modify icons

--- src/pages/result/resultTableComponents.tsx
+++ src/pages/result/resultTableComponents.tsx
@@ -1,15 +1,14 @@
 import { QuestionData } from '../../api/getQuestions'
+import { QuestionIcon } from '../../components/question/QuestionIcon'
 import { deriveClasses } from '../../util/deriveClasses'
 
 export const ResultHeader = () => (
-   //should it be a column for correct answer or not?
-   //maybe it's better to have 4 answers, green for correct one, red for wrong one
-   //
    <thead>
       <tr>
          <th rowSpan={2}>#</th>
          <th rowSpan={2}>Question</th>
          <th colSpan={4}>Answers</th>
+         <th rowSpan={2}>Category</th>
          <th rowSpan={2}>Difficulty</th>
       </tr>
       <tr>
@@ -24,22 +23,43 @@ interface RowProps {
    questionNum: number
 }
 export const ResultRow = ({ data, questionNum }: RowProps) => {
+   const {
+      question,
+      correct_answer,
+      userAnswer,
+      incorrect_answers,
+      type,
+      category,
+      difficulty,
+   } = data
+
+   let questionFontSizeClass = 'text-[16px]'
+
+   if (question.length > 50) {
+      questionFontSizeClass = 'text-[12px]'
+   } else if (question.length > 30) {
+      questionFontSizeClass = 'text-[14px]'
+   }
+
    return (
       <tr>
          <td>{questionNum + 1}</td>
-         <td className='question'>{data.question}</td>
+         <td className={`question ${questionFontSizeClass}`}>{question}</td>
          <td
             className={deriveClasses({
-               green: data.correct_answer == data?.userAnswer,
+               green: correct_answer == userAnswer,
             })}
          >
-            {data.correct_answer}
+            {correct_answer}
          </td>
-         {data.type == 'multiple' ? (
-            data.incorrect_answers.map((ia) => (
+         {type == 'multiple' ? (
+            incorrect_answers.map((ia) => (
                <td
                   className={deriveClasses({
-                     red: ia == data?.userAnswer,
+                     red: ia == userAnswer,
+                     ['text-[14px]']: true,
+                     ['text-[12px]']: ia.length > 30,
+                     ['text-[10px]']: ia.length > 50,
                   })}
                >
                   {ia}
@@ -49,13 +69,16 @@ export const ResultRow = ({ data, questionNum }: RowProps) => {
             <td
                colSpan={3}
                className={deriveClasses({
-                  red: data.incorrect_answers[0] == data?.userAnswer,
+                  red: incorrect_answers[0] == userAnswer,
                })}
             >
-               {data.incorrect_answers[0]}
+               {incorrect_answers[0]}
             </td>
          )}
-         <td>{data.difficulty}</td>
+         <td>
+            <QuestionIcon category={category} className='w-[50px] m-auto' />
+         </td>
+         <td className='text-[12px]'>{difficulty}</td>
       </tr>
    )
 }
